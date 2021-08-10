a00489018f0e79819b560df6ec7ffe9057bc096e
Selvia Rubiyanti <selvia208@gmil.com>
2021-08-10 14:01:28 +0700

    initial commit

---
+++ coba.R
@@ -0,0 +1,13 @@
+#minimasi linier programming
+library(lpSolve)
+#input fungsi tujuan
+f.tujuan<-c(170000,300000)
+#input matriks
+matriks<-matrix(c(27,35,10,20,150,271),nrow = 3,byrow = TRUE)
+matriks
+#input tanda pertidaksamaan
+f.dir<-c(">=","<=",">=")
+#input nilai batasan
+f.rhs<-c(0,80,500)
+#solving
+lp("min",f.tujuan,matriks,f.dir,f.rhs,compute.sens = TRUE)
